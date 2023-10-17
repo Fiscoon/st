c5495b23205980d01da1866fafda4c167ce23701
ness <ness@thinkpadx220i.my.domain>
2023-10-17 16:09:14 -0300

    Force UTF-8

--- x.c
+++ x.c
@@ -2084,7 +2084,7 @@ run:
 	if (!opt_title)
 		opt_title = (opt_line || !opt_cmd) ? "st" : opt_cmd[0];
 
-	setlocale(LC_CTYPE, "");
+	setlocale(LC_CTYPE, "en_US.UTF-8");
 	XSetLocaleModifiers("");
 	cols = MAX(cols, 1);
 	rows = MAX(rows, 1);
